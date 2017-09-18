fcd035f6e276c93bfdf885db37ce61fce0647e86
srikanthpragada <srikanthpragada@gmail.com>
2017-09-18 11:36:22 +0530

    18-SEP

---
+++ Controllers/HelloController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace mvcdemo.Controllers
+{
+    public class HelloController : Controller
+    {
+        // Hello/Index?name=Srikanth
+        // Test/Srikanth 
+        public ActionResult Index(string name)
+        {
+            ViewBag.Name = name;
+            ViewBag.Message = "Today is : " + DateTime.Now.ToLongDateString();
+            ViewBag.Author = "Srikanth Pragada";
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+    }
+}
