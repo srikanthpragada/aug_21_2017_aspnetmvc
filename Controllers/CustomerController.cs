ae9e44e4862e69df85a89bdb70f27b0829d9d11d
srikanthpragada <srikanthpragada@gmail.com>
2017-09-20 09:31:18 +0530

    20-SEP

---
+++ Controllers/CustomerController.cs
@@ -0,0 +1,40 @@
+﻿using mvcdemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace mvcdemo.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: Customer
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Add()
+        {
+            Customer cust = new Customer();
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Add(Customer cust)
+        {
+            if (cust.Age < 18)
+                ModelState.AddModelError("Age", "Age must be greater than 18");
+
+            if (ModelState.IsValid)
+            {
+                ViewBag.Message = "Customer Has Been Added Successfully!";
+            }
+            return View(cust);  // Go back to same view
+
+            
+
+
+        }
+    }
+}
---
+++ Models/Customer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace mvcdemo.Models
+{
+    public class Customer
+    {
+        [Required]
+        [StringLength(30,MinimumLength = 5,ErrorMessage = "Name must be atleast 5 chars")]
+        public String Name { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public String Email { get; set; }
+
+        [Required]
+        [RegularExpression("^[0-9]{10}$", ErrorMessage ="Mobile number must be 10 digits")]
+        public String Mobile { get; set; }
+
+
+        public int Age { get; set; }
+
+    }
+}
