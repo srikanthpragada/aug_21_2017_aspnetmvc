dbc74c1f1b875f46e5d8e047025a3bae6dd52790
srikanthpragada <srikanthpragada@gmail.com>
2017-09-22 09:30:05 +0530

    22-SEP

--- Controllers/AjaxController.cs
+++ Controllers/AjaxController.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using mvcdemo.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Web;
 using System.Web.Mvc;
 
@@ -8,7 +10,12 @@ namespace mvcdemo.Controllers
 {
     public class AjaxController : Controller
     {
-        // GET: Ajax
+        public ActionResult Controls()
+        {
+            return View();
+        }
+
+        
         public ActionResult Index()
         {
             return View();
@@ -17,6 +24,21 @@ namespace mvcdemo.Controllers
         public string Now()
         {
             return DateTime.Now.ToString();
+        }
+
+        public  ActionResult Search(string pattern)
+        {
+            // Thread.Sleep(3000);
+            var books = BookRepository.GetBooks()
+                        .Where(b => b.Title.ToUpper().Contains(pattern.ToUpper()));
+
+            //if (books.Count() > 0)
+            //    return PartialView("_books", books);
+            //else
+            //    return Content("<h3>Sorry! No books found!</h3>");
+
+            return  PartialView("_books", books);
+
         }
     }
 }
---
+++ Models/BookRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace mvcdemo.Models
+{
+    public class BookRepository
+    {
+        public static Book [] GetBooks()
+        {
+            return new Book[] {
+                  new Book { Title = "Pro Asp.Net MVC", Price = 890, Author = "Adam Freeman" },
+                  new Book { Title = "C# Complete Reference", Price = 590, Author = "Herebert Schildt"},
+                  new Book { Title = "Entity Framework", Price = 670, Author ="Julia Leeman"},
+                  new Book { Title = "Asp.Net Mvc", Price = 570, Author ="Scott Guthrie"},
+                  new Book { Title = "Angular", Price = 450, Author ="Tim Lee"}
+            };
+        }
+    }
+}
